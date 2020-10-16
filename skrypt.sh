f4d4e54061dcdcfde341efdc6c4401cd3dcf0e0e
cefeon <cefeon@outlook.com>
2020-10-17 00:11:48 +0200

    added dir1 to copy and move string

--- skrypt.sh
+++ skrypt.sh
@@ -11,15 +11,15 @@ for file in $dir1/*
 do
    if [ -f $file ]
    then
-      f_name=${file%.*} #Remove all chars from end to first .
+      f_name=${file%.*} #Remove all chars from end to first . (with .)
       f_name=${f_name##*/} #Remove all chars from start to last /
-      f_ext=${file##*.} #Remove all chars from start to last .
+      f_ext=${file##*.} #Remove all chars from start to last . (with .)
       if [[ $f_name == *"$pattern"* ]]
       then
          if [[ ".$f_ext" == "$ext1" ]]
          then
-            cp $f_name$ext1 $f_name$ext2
-            mv $f_name$ext2 $dir2
+            cp $dir1/$f_name$ext1 $f_name$ext2
+            mv $dir1/$f_name$ext2 $dir2/
          fi
       fi
    fi
